314d46b7af57fa206f73b17bd866fbd19e1c1931
DanieliSanto0s <danielivitoria93taqu@gmail.com>
2025-03-14 18:56:53 -0300

    feat: Adicionando a ultima seção da landing page e iniciando footer

---
+++ Oportuniza.Application/src/app/pages/landing-page/bloco-acao/bloco-acao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bloco-acao',
+  imports: [],
+  templateUrl: './bloco-acao.component.html',
+  styleUrl: './bloco-acao.component.css'
+})
+export class BlocoAcaoComponent {
+
+}
--- Oportuniza.Application/src/app/pages/landing-page/landing-page.component.ts
+++ Oportuniza.Application/src/app/pages/landing-page/landing-page.component.ts
@@ -5,10 +5,12 @@ import { SobreComponent } from './sobre/sobre.component';
 import { ComofuncionaComponent } from './comofunciona/comofunciona.component';
 import { VagasComponent } from './vagas/vagas.component';
 import { DepoimentosComponent } from './depoimentos/depoimentos.component';
+import { BlocoAcaoComponent } from './bloco-acao/bloco-acao.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [HeaderComponent, HeroComponent, SobreComponent, ComofuncionaComponent, VagasComponent, DepoimentosComponent],
+  imports: [HeaderComponent, HeroComponent, SobreComponent, ComofuncionaComponent, VagasComponent, DepoimentosComponent, BlocoAcaoComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
