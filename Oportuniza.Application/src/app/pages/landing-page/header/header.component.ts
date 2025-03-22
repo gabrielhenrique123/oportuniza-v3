ef842c2ffc8883e4f8570c51c420ab6ece1377e5
DanieliSanto0s <danielivitoria93taqu@gmail.com>
2025-03-22 20:26:10 -0300

    Versão final do design

--- Oportuniza.Application/src/app/pages/landing-page/header/header.component.ts
+++ Oportuniza.Application/src/app/pages/landing-page/header/header.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
+  lastScrollTop = 0;
+  isHeaderHidden = false;
+  scrollThreshold = 100;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > this.lastScrollTop && scrollTop > this.scrollThreshold) {
+      this.isHeaderHidden = true;
+    } else if (scrollTop < this.lastScrollTop) {
+      this.isHeaderHidden = false;
+    }
+
+    this.lastScrollTop = scrollTop;
+  }
 
 }
