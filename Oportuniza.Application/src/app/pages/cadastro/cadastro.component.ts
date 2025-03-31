0e726eb2806280d5f0791845d1ac9faa4ed161bc
gabrielhenrique123 <gabrielhenrique123>
2025-03-31 04:24:14 -0300

    Descrição das mudanças feitas no projeto

--- Oportuniza.Application/src/app/pages/cadastro/cadastro.component.ts
+++ Oportuniza.Application/src/app/pages/cadastro/cadastro.component.ts
@@ -1,28 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css'],
 })
-
 export class CadastroComponent {
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
   acceptTerms: boolean = false;
   passwordVisible: boolean = false;
+  confirmPasswordVisible: boolean = false;
+
+  constructor(private router: Router) {
+    this.restoreTermsAcceptance();
+  }
 
   togglePassword() {
     this.passwordVisible = !this.passwordVisible;
   }
+  toggleConfirmPassword() {
+    this.confirmPasswordVisible = !this.confirmPasswordVisible;
+  }
 
   register() {
+    if (!this.email || !this.validateEmail(this.email)) {
+      alert('Por favor, insira um e-mail válido.');
+      return;
+    }
+
+    if (this.password.length < 6) {
+      alert('A senha deve ter pelo menos 6 caracteres.');
+      return;
+    }
+
     if (this.password !== this.confirmPassword) {
       alert('As senhas não coincidem.');
       return;
@@ -34,5 +51,24 @@ export class CadastroComponent {
     }
 
     console.log('Cadastro realizado com sucesso:', { email: this.email, password: this.password });
+
+    this.router.navigate(['/primeira-etapa']);
+  }
+
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  saveTermsAcceptance() {
+    localStorage.setItem('acceptTerms', JSON.stringify(this.acceptTerms));
+  }
+
+
+  restoreTermsAcceptance() {
+    const storedAcceptance = localStorage.getItem('acceptTerms');
+    if (storedAcceptance) {
+      this.acceptTerms = JSON.parse(storedAcceptance);
+    }
   }
 }
